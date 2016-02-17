a48c7cedd375b09ed6e2e1c49fdca6c8735f7e62
mAAdhaTTah <jamesorodig@gmail.com>
2016-02-16 21:29:28 -0500

    Add EntityManager Contract
    This is a starting point for discussing what features should
    be including in the database API.

---
+++ src/Contract/Database/EntityManager.php
@@ -0,0 +1,59 @@
+<?php
+namespace Intraxia\Jaxion\Contract\Database;
+
+use Intraxia\Jaxion\Collection\Base as Collection;
+use Intraxia\Jaxion\Model\Base as Model;
+use WP_Error;
+
+interface EntityManager {
+	/**
+	 * Get a single model of the provided class with the given ID.
+	 *
+	 * @param string $class Fully qualified class name of model.
+	 * @param int    $id    ID of the model.
+	 *
+	 * @return Model|WP_Error
+	 */
+	public function find( $class, $id );
+
+	/**
+	 * Finds all the models of the provided class for the given params.
+	 *
+	 * This method will return an empty Collection if the query returns no models.
+	 *
+	 * @param string $class  Fully qualified class name of models to find.
+	 * @param array  $params Params to constrain the find.
+	 *
+	 * @return Collection|WP_Error
+	 */
+	public function find_by( $class, $params = array() );
+
+	/**
+	 * Saves a new model of the provided class with the given data.
+	 *
+	 * @param string $class
+	 * @param array  $data
+	 *
+	 * @return Model|WP_Error
+	 */
+	public function create( $class, $data = array() );
+
+	/**
+	 * Updates a model with its latest dataE.
+	 *
+	 * @param Model $model
+	 *
+	 * @return Model|WP_Error
+	 */
+	public function persist( Model $model );
+
+	/**
+	 * Delete the provide
+	 *
+	 * @param Model $model
+	 * @param bool  $force
+	 *
+	 * @return mixed
+	 */
+	public function delete( Model $model, $force = false );
+}
